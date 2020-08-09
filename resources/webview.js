d62b07be722204234e27456b9fb77fd75de17ec3
Daxit Agarwal <daxit@users.noreply.github.com>
2020-08-08 22:38:42 -0700

    Migrate webview toolbar

--- resources/webview.js
+++ resources/webview.js
@@ -1,14 +1,22 @@
 // disable the context menu (eg. the right click menu) to have a more native feel
-document.addEventListener('contextmenu', (e) => {
-  e.preventDefault()
-})
+document.addEventListener('contextmenu', e => {
+  e.preventDefault();
+});
 
 // call the plugin from the webview
-document.getElementById('button').addEventListener('click', () => {
-  window.postMessage('nativeLog', 'Called from the webview')
-})
-
-// call the wevbiew from the plugin
-window.setRandomNumber = (randomNumber) => {
-  document.getElementById('answer').innerHTML = 'Random number from the plugin: ' + randomNumber
+var actions = document.getElementsByClassName('action');
+for (let i = 0; i < actions.length; i++) {
+  actions[i].addEventListener('click', () => {
+    // document.getElementById('answer').innerHTML = 'Action Taken: ' + actions[i].id + ' Comment(s)';
+    window.postMessage(actions[i].id, 'Action Taken: ' + actions[i].id + ' Comment(s)');
+  });
 }
+
+document.getElementById('close').addEventListener('click', () => {
+  window.postMessage('close', 'Window closed.');
+});
+
+// call the webview from the plugin
+// window.setRandomNumber = (randomNumber) => {
+//   document.getElementById('answer').innerHTML = 'Random number from the plugin: ' + randomNumber
+// }
