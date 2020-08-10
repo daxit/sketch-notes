79ae1d3669ac0bd0fb6c7e30dc347acac16708cf
Daxit Agarwal <daxit@users.noreply.github.com>
2020-08-09 20:20:14 -0700

    Correct, error check, and document create command

--- src/actions/create-comment.js
+++ src/actions/create-comment.js
@@ -1,61 +1,46 @@
-import sketch from 'sketch';
+import { Rectangle, Group, UI, Settings, getSelectedDocument } from 'sketch';
 import { Comment } from '../model/Comment.js';
 import { renderComments, createMarker } from '../util.js';
 
-// Object definitions
-var Rectangle = require('sketch/dom').Rectangle;
-var Group = require('sketch/dom').Group;
-var UI = require('sketch/ui');
-var Settings = require('sketch/settings');
-
 export default function () {
-  const selectedObject = sketch.getSelectedDocument().selectedLayers.layers[0];
-  var context = Settings.layerSettingForKey(selectedObject.getParentArtboard(), 'context');
-
-  var valid = true;
-  UI.getInputFromUser(
-    'Enter comment:',
-    {
-      initialValue: 'Type a comment...',
-      numberOfLines: 3
-    },
-    (err, value) => {
-      if (err) valid = false; //most likely canceled, set to not valid
-      context.commentList.push(new Comment(value, selectedObject.id));
-    }
-  );
-  if (!valid) {
+  const document = getSelectedDocument();
+  if (document.selectedLayers.length > 1) {
+    UI.message('Select one layer or group');
     return;
   }
+  const selection = document.selectedLayers.layers[0];
+  const board = selection.getParentArtboard();
+  // Get comment from user
+  let comment = null;
+  UI.getInputFromUser(
+    'Enter comment:',
+    { initialValue: 'Type a comment here...', numberOfLines: 3 },
+    (err, value) => (comment = err ? null : value)
+  );
+  if (comment === null) return;
+
+  let context = Settings.layerSettingForKey(board, 'context');
+  // Create a new comment linked to the selection
+  let length = context.commentList.push(new Comment(comment, selection.id));
 
   // Encapsulate selected object in a group and create a marker for it
-  var note = new Group({
-    name: selectedObject.name + '_note',
-    parent: selectedObject.parent,
-    index: selectedObject.index,
+  let note = new Group({
+    name: selection.name + '_note',
+    parent: selection.parent,
+    index: selection.index,
     frame: new Rectangle(
-      selectedObject.frame.x - 10,
-      selectedObject.frame.y - 10,
-      selectedObject.frame.width + 10,
-      selectedObject.frame.height + 10
+      selection.frame.x - 10,
+      selection.frame.y - 10,
+      selection.frame.width + 10,
+      selection.frame.height + 10
     )
   });
   // Reparent selected object and reframe it relative to its new parent
-  selectedObject.parent = note;
-  selectedObject.frame = new Rectangle(
-    10,
-    10,
-    selectedObject.frame.width,
-    selectedObject.frame.height
-  );
-  context.commentList[context.commentList.length - 1].markerID = createMarker(
-    context.commentList.length,
-    note
-  );
-
-  renderComments(
-    context.commentList,
-    sketch.getSelectedDocument().getLayerWithID(context.commentsContainerID)
-  );
-  Settings.setLayerSettingForKey(selectedObject.getParentArtboard(), 'context', context);
+  selection.parent = note;
+  selection.frame = new Rectangle(10, 10, selection.frame.width, selection.frame.height);
+  // Create a marker and store its ID
+  context.commentList[length - 1].markerID = createMarker(length, note);
+  // Render the comments on the notes panel
+  renderComments(context.commentList, document.getLayerWithID(context.commentsContainerID));
+  Settings.setLayerSettingForKey(board, 'context', context);
 }
