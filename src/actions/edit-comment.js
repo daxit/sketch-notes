7aac5f4c25a46011bc5895d29eb37bc35cac3786
Daxit Agarwal <daxit@users.noreply.github.com>
2020-08-09 20:15:32 -0700

    Refactor edit command

--- src/actions/edit-comment.js
+++ src/actions/edit-comment.js
@@ -2,28 +2,23 @@ import { UI, Settings, getSelectedDocument } from 'sketch';
 import { renderComments } from '../util.js';
 
 export default function () {
-  if (getSelectedDocument().selectedLayers.length > 1) {
+  const document = getSelectedDocument();
+  if (document.selectedLayers.length > 1) {
     UI.message('Select one layer or group');
     return;
   }
-  const selection = getSelectedDocument().selectedLayers.layers[0];
-  let context = Settings.layerSettingForKey(selection.getParentArtboard(), 'context');
-  let comment = context.commentList.find(comment => comment.subjectID === selection.layers[0].id);
+  const selection = document.selectedLayers.layers[0];
+  const context = Settings.layerSettingForKey(selection.getParentArtboard(), 'context');
+  const comment = context.commentList.find(comment => comment.subjectID === selection.layers[0].id);
   if (!comment) {
     UI.message('Layer does not have a comment');
     return;
   }
   UI.getInputFromUser(
     'Update comment:',
-    {
-      initialValue: comment.comment,
-      numberOfLines: 3
-    },
+    { initialValue: comment.comment, numberOfLines: 3 },
     (err, value) => (comment.comment = err ? comment.comment : value)
   );
-  renderComments(
-    context.commentList,
-    getSelectedDocument().getLayerWithID(context.commentsContainerID)
-  );
+  renderComments(context.commentList, document.getLayerWithID(context.commentsContainerID));
   Settings.setLayerSettingForKey(selection.getParentArtboard(), 'context', context);
 }
